669e5094a461747ef51666a7d0d068ba94d3407e
armanibash2 <153381808+armanibash2@users.noreply.github.com>
2024-01-18 16:27:25 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -94,7 +94,7 @@ export default {
 						});
 					}
 					default:
-						url.hostname = 'www.setareyek.ir';
+						url.hostname = 'www.zula.ir';
 						url.protocol = 'https:';
 						request = new Request(url, request);
 						return await fetch(request);
